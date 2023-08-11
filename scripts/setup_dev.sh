6ca6d8e7be00168423acaa7bea063edeaa51040e
mflinn-broad <mflinn@broadinstitute.org>
2023-08-11 15:10:29 -0400

    support for github auth

---
+++ scripts/setup_dev.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+# This script is used to setup the environment for the project.
+export POSTGRES_HOST=localhost
+export POSTGRES_PORT='5432'
+export POSTGRES_USER=postgres
+export POSTGRES_PASSWORD=password
+export BACKSTAGE_AUTH_GITHUB_CLIENT_ID=$(VAULT_ADDR='https://clotho.broadinstitute.org:8200' vault read -field=clientId secret/suitable/backstage/local/github-oauth)
+export BACKSTAGE_AUTH_GITHUB_CLIENT_SECRET=$(VAULT_ADDR='https://clotho.broadinstitute.org:8200' vault read -field=clientSecret secret/suitable/backstage/local/github-oauth)
+
+concurrently "yarn start" "yarn start-backend"
