56609da9da97364b3e8dcd624b40787cd3166b9d
mflinn-broad <60187023+mflinn-broad@users.noreply.github.com>
2023-08-14 15:12:16 -0400

    Fix Github API throttling on broadbot for Github Backstage Entity Auto-discovery (#10)
    * fix github enitity discovey throttling
    
    * fix prettier lints

--- packages/backend/src/plugins/catalog.ts
+++ packages/backend/src/plugins/catalog.ts
@@ -1,6 +1,6 @@
 import { CatalogBuilder } from '@backstage/plugin-catalog-backend';
 import { ScaffolderEntitiesProcessor } from '@backstage/plugin-scaffolder-backend';
-// import { GithubEntityProvider } from '@backstage/plugin-catalog-backend-module-github';
+import { GithubEntityProvider } from '@backstage/plugin-catalog-backend-module-github';
 import { Router } from 'express';
 import { PluginEnvironment } from '../types';
 
@@ -8,16 +8,17 @@ export default async function createPlugin(
   env: PluginEnvironment,
 ): Promise<Router> {
   const builder = await CatalogBuilder.create(env);
-  // builder.addEntityProvider(
-  //   GithubEntityProvider.fromConfig(env.config, {
-  //     logger: env.logger,
-  //     schedule: env.scheduler.createScheduledTaskRunner({
-  //       frequency: {minutes: 30},
-  //       timeout: {minutes: 3},
-  //     }),
-  //     scheduler: env.scheduler,
-  //   }),
-  // );
+  builder.addEntityProvider(
+    GithubEntityProvider.fromConfig(env.config, {
+      logger: env.logger,
+      schedule: env.scheduler.createScheduledTaskRunner({
+        frequency: { hours: 6 },
+        timeout: { minutes: 3 },
+        initialDelay: { minutes: 5 },
+      }),
+      scheduler: env.scheduler,
+    }),
+  );
   builder.addProcessor(new ScaffolderEntitiesProcessor());
   const { processingEngine, router } = await builder.build();
   await processingEngine.start();
--- scripts/setup_dev.sh
+++ scripts/setup_dev.sh
@@ -8,5 +8,6 @@ export POSTGRES_PASSWORD=password
 export BACKSTAGE_AUTH_GITHUB_CLIENT_ID=$(VAULT_ADDR='https://clotho.broadinstitute.org:8200' vault read -field=clientId secret/suitable/backstage/local/github-oauth)
 export BACKSTAGE_AUTH_GITHUB_CLIENT_SECRET=$(VAULT_ADDR='https://clotho.broadinstitute.org:8200' vault read -field=clientSecret secret/suitable/backstage/local/github-oauth)
 export BACKSTAGE_GITHUB_TOKEN=$(VAULT_ADDR='https://clotho.broadinstitute.org:8200' vault read -field=token secret/suitable/backstage/common/github-token)
+export LOG_LEVEL="${LOG_LEVEL:-debug}"
 
 concurrently "yarn start" "yarn start-backend"
