c31e0ee4d726ab1b3d982fd9a9af194cccd1c983
mflinn-broad <60187023+mflinn-broad@users.noreply.github.com>
2023-08-16 10:19:15 -0400

    Add Github User and teams provider and update foundation meta (#11)
    * fix github enitity discovey throttling
    
    * fix prettier lints
    
    * ingest user and teams from github and update foundation.yaml
    
    * update job schedule
    
    * run prettier

--- packages/backend/src/plugins/catalog.ts
+++ packages/backend/src/plugins/catalog.ts
@@ -1,6 +1,9 @@
 import { CatalogBuilder } from '@backstage/plugin-catalog-backend';
 import { ScaffolderEntitiesProcessor } from '@backstage/plugin-scaffolder-backend';
-import { GithubEntityProvider } from '@backstage/plugin-catalog-backend-module-github';
+import {
+  GithubEntityProvider,
+  GithubOrgEntityProvider,
+} from '@backstage/plugin-catalog-backend-module-github';
 import { Router } from 'express';
 import { PluginEnvironment } from '../types';
 
@@ -17,6 +20,28 @@ export default async function createPlugin(
         initialDelay: { minutes: 5 },
       }),
       scheduler: env.scheduler,
+    }),
+  );
+  builder.addEntityProvider(
+    GithubOrgEntityProvider.fromConfig(env.config, {
+      logger: env.logger,
+      id: 'databiosphereOrgProvider',
+      orgUrl: 'https://github.com/DataBiosphere',
+      schedule: env.scheduler.createScheduledTaskRunner({
+        frequency: { hours: 6 },
+        timeout: { minutes: 3 },
+        initialDelay: { minutes: 5 },
+      }),
+    }),
+    GithubOrgEntityProvider.fromConfig(env.config, {
+      logger: env.logger,
+      id: 'broadinstituteOrgProvider',
+      orgUrl: 'https://github.com/broadinstitute',
+      schedule: env.scheduler.createScheduledTaskRunner({
+        frequency: { hours: 6 },
+        timeout: { minutes: 3 },
+        initialDelay: { minutes: 5 },
+      }),
     }),
   );
   builder.addProcessor(new ScaffolderEntitiesProcessor());
--- scripts/setup_dev.sh
+++ scripts/setup_dev.sh
@@ -7,7 +7,7 @@ export POSTGRES_USER=postgres
 export POSTGRES_PASSWORD=password
 export BACKSTAGE_AUTH_GITHUB_CLIENT_ID=$(VAULT_ADDR='https://clotho.broadinstitute.org:8200' vault read -field=clientId secret/suitable/backstage/local/github-oauth)
 export BACKSTAGE_AUTH_GITHUB_CLIENT_SECRET=$(VAULT_ADDR='https://clotho.broadinstitute.org:8200' vault read -field=clientSecret secret/suitable/backstage/local/github-oauth)
-export BACKSTAGE_GITHUB_TOKEN=$(VAULT_ADDR='https://clotho.broadinstitute.org:8200' vault read -field=token secret/suitable/backstage/common/github-token)
+export BACKSTAGE_GITHUB_TOKEN=$(VAULT_ADDR='https://clotho.broadinstitute.org:8200' vault read -field=mf-dev secret/suitable/backstage/common/github-token)
 export LOG_LEVEL="${LOG_LEVEL:-debug}"
 
 concurrently "yarn start" "yarn start-backend"
