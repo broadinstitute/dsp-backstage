e82676f8c9b514607de241083e1bb67cb9e19e2b
mflinn-broad <mflinn@broadinstitute.org>
2023-08-17 11:37:05 -0400

    improve local dev flow, update readme and pg package json scripts

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# foundation
-Foundation is DSP-DevOps Internal Developer Platform Framework (Spotify Backstage by another name)
+# DSP Backstage
+
+DSP Backstage provides a suite of internal developer and platform tooling to improve the experience
+and productive of Teams contributing to Terra and DSP's other platforms.
+
+## Docker Images
+
+Docker Images for dsp-backstage are available at `us-central1-docker.pkg.dev/dsp-artifact-registry/backstage/backstage:latest`
+
+## Helm Chart
+
+A helm chart for installing this backstage deployment is also available
+`helm repo add terra-helm https://terra-helm.storage.googleapis.com/ && helm install terra-helm/backstage`
+This helm chart's defaults are tuned to run in GKE environments managed by the DSP Devops Team. If running in another
+environment, additional confiugration may be needed. Documentation for this helmchart is a available [here](https://github.com/broadinstitute/terra-helmfile/tree/master/charts/backstage)
+
--- scripts/setup_dev.sh
+++ scripts/setup_dev.sh
@@ -1,13 +1,25 @@
 #!/usr/bin/env sh
 
 # This script is used to setup the environment for the project.
-export POSTGRES_HOST=localhost
-export POSTGRES_PORT='5432'
-export POSTGRES_USER=postgres
-export POSTGRES_PASSWORD=password
-export BACKSTAGE_AUTH_GITHUB_CLIENT_ID=$(VAULT_ADDR='https://clotho.broadinstitute.org:8200' vault read -field=clientId secret/suitable/backstage/local/github-oauth)
-export BACKSTAGE_AUTH_GITHUB_CLIENT_SECRET=$(VAULT_ADDR='https://clotho.broadinstitute.org:8200' vault read -field=clientSecret secret/suitable/backstage/local/github-oauth)
-export BACKSTAGE_GITHUB_TOKEN=$(VAULT_ADDR='https://clotho.broadinstitute.org:8200' vault read -field=mf-dev secret/suitable/backstage/common/github-token)
-export LOG_LEVEL="${LOG_LEVEL:-debug}"
+export POSTGRES_HOST="${POSTGRES_HOST:-localhost}"
+export POSTGRES_PORT="${POSTGRES_PORT:-5432}"
+export POSTGRES_USER="${POSTGRES_USER:-backstage}"
+export POSTGRES_PASSWORD="${POSTGRES_PASSWORD:-password}"
+export BACKSTAGE_AUTH_GITHUB_CLIENT_ID="$(VAULT_ADDR='https://clotho.broadinstitute.org:8200' vault read -field=clientId secret/suitable/backstage/local/github-oauth)"
+export BACKSTAGE_AUTH_GITHUB_CLIENT_SECRET="$(VAULT_ADDR='https://clotho.broadinstitute.org:8200' vault read -field=clientSecret secret/suitable/backstage/local/github-oauth)"
+export BACKSTAGE_GITHUB_TOKEN="$(VAULT_ADDR='https://clotho.broadinstitute.org:8200' vault read -field=mf-dev secret/suitable/backstage/common/github-token)"
+export LOG_LEVEL="${LOG_LEVEL:-info}"
+
+# if docker container is not running start it
+pg_container_running=$(docker inspect -f '{{.State.Running}}' backstage-pg)
+if [ $? -eq 1 ]; then
+  echo "Docker container backstage-pg does not exist. Running it now..."
+  docker run --name backstage-pg -e POSTGRES_PASSWORD="${POSTGRES_PASSWORD}" -e "POSTGRES_USER=${POSTGRES_USER}" -d -p "${POSTGRES_PORT}:${POSTGRES_PORT}" postgres
+fi
+
+if [ "${pg_container_running}" = "false" ]; then
+  echo "Docker container backstage-pg exists but is not running. Starting it now..."
+  docker start backstage-pg
+fi
 
 concurrently "yarn start" "yarn start-backend"
