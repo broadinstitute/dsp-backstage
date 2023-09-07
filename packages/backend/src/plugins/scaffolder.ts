3602676dd00fe01db7c2673e8c2c7a7dd4665d34
mflinn-broad <mflinn@broadinstitute.org>
2023-09-07 14:43:11 -0400

    setup skeleton for new scaffolder action

--- packages/backend/src/plugins/scaffolder.ts
+++ packages/backend/src/plugins/scaffolder.ts
@@ -1,7 +1,9 @@
 import { CatalogClient } from '@backstage/catalog-client';
-import { createRouter } from '@backstage/plugin-scaffolder-backend';
+import { createBuiltinActions, createRouter } from '@backstage/plugin-scaffolder-backend';
 import { Router } from 'express';
+import { ScmIntegrations } from '@backstage/integration';
 import type { PluginEnvironment } from '../types';
+import { createGithubTerraformAction } from '../../../../plugins/scaffolder-backend-module-githubterraform/src';
 
 export default async function createPlugin(
   env: PluginEnvironment,
@@ -9,8 +11,18 @@ export default async function createPlugin(
   const catalogClient = new CatalogClient({
     discoveryApi: env.discovery,
   });
+  const integrations = ScmIntegrations.fromConfig(env.config);
+  const builtinActions = createBuiltinActions({
+    integrations,
+    catalogClient,
+    config: env.config,
+    reader: env.reader,
+  })
+
+  const actions = [...builtinActions, createGithubTerraformAction()];
 
   return await createRouter({
+    actions,
     logger: env.logger,
     config: env.config,
     database: env.database,
---
+++ plugins/scaffolder-backend-module-githubterraform/src/actions/githubTerrform/index.ts
@@ -0,0 +1,1 @@
+export { createGithubTerraformAction } from './githubTerraform';
